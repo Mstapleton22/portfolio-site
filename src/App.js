d40e8100db690e1a73d3be94986754b27e387e41
Mstapleton22 <mike.stapleton2@gmail.com>
2019-03-10 13:59:58 -0600

    routing created between pages and home stylying

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import Resume from './components/Resume';
+import Projects from './components/Projects';
+import Nav from './components/Nav';
 import './App.css';
 
+
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      // customers: [],
+      // navSearch: false,
+      // isLoggedIn: false
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <div className="App">
+            <Nav />
+            <Route path="/" exact component={Home} />
+            <Route path="/about" exact component={About} />
+            <Route path="/resume" exact component={Resume} />
+            <Route path="/projects" exact component={Projects} />
+            {/* <Route path="/sPark-userBook" component={userBook} />
+            <Route path="/sPark-login" render={() => <Login
+              navSearch={this.state.navSearch}
+              isLoggedIn={this.isLoggedIn}
+            />} /> */}
+            {/* <Route path="/sPark-space-manager-page" render={() => <SpaceManagerList isLoggedIn={this.state.isLoggedIn} customers={this.state.customers} />} /> */}
+          </div>
+        </Router>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'font-awesome/css/font-awesome.css'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
