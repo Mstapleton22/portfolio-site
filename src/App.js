8e81f6ba18b7806603a8526bdf9ed29bb60c8d25
Mstapleton22 <mike.stapleton2@gmail.com>
2019-03-22 16:56:30 -0600

    working on gallery

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './components/Home';
 import About from './components/About';
-import Resume from './components/Resume';
+// import Resume from './components/Resume';
 import Projects from './components/Projects';
 import Nav from './components/Nav';
 import './App.css';
@@ -32,7 +32,7 @@ class App extends Component {
               toggleHidden={this.toggleHidden}
               isHidden={this.state.isHidden}
             />
-            <Route path="/resume" exact component={Resume} />
+            {/* <Route path="/resume" exact component={Resume} /> */}
             <Route path="/projects" exact component={Projects} />
           </div>
         </Router>
